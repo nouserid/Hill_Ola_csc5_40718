23f91bdbbcdff4fbca2acda29d62356b7042daf6
nouserid <ohill@student.rcc.edu>
2015-01-13 18:58:40 -0800

    Signed-off-by: nouserid <ohill@student.rcc.edu>

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 13, 2015, 6:46 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<< "Hello World " <<endl;
+    return 0;
+}
+
